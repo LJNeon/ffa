754c3bcd43c1c6ed33e0fb23e88a2fc6c061c9f7
El Jay <ljninjaa@gmail.com>
2018-07-08 11:00:31 -0700

    started merging archives and logs + checks in db

--- src/services/logs.js
+++ src/services/logs.js
@@ -23,33 +23,33 @@ const {data: {queries}} = require("./data.js");
 const time = require("../utilities/time.js");
 
 module.exports = {
-  async add(archive) {
-    const {channels: {archive_id}} = await db.getGuild(
-      archive.guild_id,
-      {channels: "archive_id"}
+  async add(log) {
+    const {channels: {log_id}} = await db.getGuild(
+      log.guild_id,
+      {channels: "log_id"}
     );
 
     const res = await db.pool.query(
       queries.addArchive,
-      [archive.guild_id,
-        archive.user_id,
-        archive.data,
+      [log.guild_id,
+        log.user_id,
+        log.data,
         time.epoch(),
-        archive.type]
+        log.type]
     );
 
-    if (archive_id != null) {
-      const channel = client.getChannel(archive_id);
+    if (log_id != null) {
+      const channel = client.getChannel(log_id);
 
       if (channel != null) {
-        const user = client.users.get(archive.user_id);
+        const user = client.users.get(log.user_id);
 
         return message.create(channel, {
           author: {
             icon_url: user.avatarURL,
-            name: `${message.tag(user)} (${archive.user_id})`
+            name: `${message.tag(user)} (${log.user_id})`
           },
-          description: await this.describe(archive),
+          description: await this.describe(log),
           footer: {text: `ID: ${res.rows[0].archive_id}`},
           timestamp: new Date()
         });
